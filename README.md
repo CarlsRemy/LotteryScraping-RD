77ececb598adcd8ee2a4b0e79c30c2263ac538df
unknown <carisynfante14@gmail.com>
2023-08-30 03:12:16 -0400

    implementacion de EndPoints

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# LotteryScraping-RD
-LotteryScraping-RD es un proyecto de web scraping diseñado para extraer datos en tiempo real de los resultados de loterías en la República Dominicana. Los datos extraídos incluyen un id, números ganadores, fecha de los sorteos y nombres de las loterías en formato JSON.
+# LotteryScraping-RD Lotería 
 
+Este proyecto es una aplicación de web scraping que extrae información sobre las loterías dominicanas desde el sitio web [Loterías Dominicanas](https://loteriasdominicanas.com/). Proporciona una API para buscar y filtrar información sobre las loterías y sus resultados.
 
 para el funcionamiento de este cargamos un archivo **json** de la siguiente manera:
 
@@ -12,21 +12,39 @@ para el funcionamiento de este cargamos un archivo **json** de la siguiente mane
 Aunque tambien lo pudemos hacer de manera dirrecta:
 
 ``` python
-	
-		json_data = json_data = '''
-		[
-			{
-				"id": 1,
-				"name": "La Primera Día"
-			},
-			{
-				"id": 2,
-				"name": "Anguila Mañana"
-			},
-			# ... otros elementos ...
-		]
-		'''
-		data = json.loads(json_data)
+	json_data = json_data = '''
+	[
+		{
+			"id": 1,
+			"name": "La Primera Día"
+		},
+		{
+			"id": 2,
+			"name": "Anguila Mañana"
+		},
+		# ... otros elementos ...
+	]
+	'''
+
+	data = json.loads(json_data)
 ```
 
-Dicho **Json** se utiliza para filtrar y seleccionar las distintas loterías de la página web, permitiendo devolver únicamente las loterías específicas que se deseen. Cada entrada en el JSON posee un identificador (ID) que es empleado para establecer un orden dentro de la estructura, y el nombre en el JSON debe coincidir con el nombre de la lotería según se presenta en la página [loteriasdominicanas](https://loteriasdominicanas.com/)
+Dicho **Json** se utiliza para filtrar y seleccionar las distintas loterías de la página web, permitiendo devolver únicamente las loterías específicas que se deseen. Cada entrada en el JSON posee un identificador (ID) que es empleado para establecer un orden dentro de la estructura, y el nombre en el JSON debe coincidir con el nombre de la lotería según se presenta en la página [Loterías Dominicanas](https://loteriasdominicanas.com/)
+
+## Características
+
+- Obtención de datos de loterías en tiempo real.
+- Búsqueda y filtrado por nombre de lotería y fecha.
+- Filtar entre consorcio de loterias ej:Nacional, leisa, americana etc.
+
+## Requisitos
+
+- Python 3.x
+- Bibliotecas: Flask, Flask-CORS, BeautifulSoup4
+
+## Instalación
+
+1. Clona el repositorio:
+
+```bash
+git clone https://github.com/CarlsRemy/LotteryScraping-RD.git
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask,jsonify
 from flask_cors import CORS, cross_origin
 import re
@@ -7,10 +8,16 @@ import os
 import json
 
 
-def load_html():
+def load_html(search_date=None):
+
 	url1 = "https://loteriasdominicanas.com/"
 	# esta Segunda Url es porque en la Pag. principal no aparecen las Loterias Anguila
 	url2 = "https://loteriasdominicanas.com/anguila" 
+
+	# Agregar el parámetro date a la URL si existe
+	if search_date:
+		url1 += f"?date={search_date}"
+		url2 += f"?date={search_date}"
         
 	# Crea una lista para almacenar los elementos de ambos soups
 	games_blocks = []
@@ -35,7 +42,29 @@ def load_html():
 	return games_blocks
 
 
-def scraping():
+def load_html_name(search_name,search_date=None):
+	url1 = f"https://loteriasdominicanas.com/{search_name}"
+
+	# Agregar el parámetro date a la URL si existe
+	if search_date:
+		url1 += f"?date={search_date}"
+        
+	# Crea una lista para almacenar los elementos de ambos soups
+	games_blocks = []
+
+	try:
+		html1 = urllib.request.urlopen(url1).read()      
+		soup1 = BeautifulSoup(html1, "html.parser")
+                
+		# Encuentra los elementos deseados del soup y agrégalos a la lista
+		blocks1 = soup1.find_all("div", class_="game-block")
+		games_blocks.extend(blocks1)
+	except:
+		return []
+
+	return games_blocks
+
+def scraping(search_date=None):
 	data = []
 	loteries_parser = []
   # Cargar JSON en un Archivo
@@ -67,11 +96,155 @@ def scraping():
 	return sorted(loteries_parser, key=lambda k:k["id"])
 
 
+def scrapingByName(search_name,search_date=None):
+	data = []
+	loteries_parser = []
+  # Cargar JSON en un Archivo
+	with open('lottery.json') as file:
+		json_data = file.read()
+		data = json.loads(json_data)
+
+	# Load HTML 
+	games_blocks = load_html_name(search_name,search_date)
+
+	for game_block in games_blocks:
+		block = {}
+		title = game_block.find("a", "game-title").getText().strip().lower()
+
+		filtered_data = [item for item in data if item["name"].lower() == title]
+		if len(filtered_data) == 0:
+			continue  
+
+		pather_score = game_block.find_all("span", "score")
+		pather_date = game_block.find("div", "session-date").getText().strip()
+		score = "-".join(span.text.strip() for span in pather_score)
+
+		block['id'] = filtered_data[0]["id"]
+		block['name'] = filtered_data[0]["name"]
+		block['date'] = pather_date
+		block['number'] = score
+		loteries_parser.append(block)
+
+	return sorted(loteries_parser, key=lambda k:k["id"])
+
+
 app = Flask(__name__)
 CORS(app)
 port = int(os.environ.get("PORT", 5000))
+
 @app.route("/")
 def search_lotery():
   return jsonify(scraping())
+
+
+@app.route("/search")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	if not search_query:
+		return jsonify({"error": "Missing 'name' parameter"}), 400
+	
+	filtered_lotteries = [lottery for lottery in scraping(search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-primera")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("la-primera",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+@app.route("/loteria-nueva-york")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("nueva-york",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+@app.route("/loteria-la-suerte")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("la-suerte-dominicana",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-lotedom")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("lotedom",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-anguila")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("anguila",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-nacional")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("loteria-nacional",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-leidsa")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("leidsa", search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+    
+@app.route("/loteria-real")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("loto-real", search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-loteka")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name", '')
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("loteka", search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
+
+@app.route("/loteria-americana")
+def search_lotery_by_name():
+	search_query = urllib.request.args.get("name")
+	search_date = urllib.request.request.args.get("date", datetime.datetime.now().strftime("%d-%m-%Y"))
+	
+	filtered_lotteries = [lottery for lottery in scrapingByName("americanas",search_date) if search_query.lower() in lottery["name"].lower()]
+	
+	return filtered_lotteries
+
 
 app.run(port=port)
